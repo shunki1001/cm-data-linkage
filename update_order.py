3f53f4cc4e3852924ab82ea0008af2334da9c46b
Shunki <tajimura0505@gmail.com>
2024-07-09 20:21:45 +0900

    クロスモールへのAPIを関数で共通化

--- update_order.py
+++ update_order.py
@@ -1,75 +1,21 @@
 # %%
-import hashlib
 import os
-import re
 import time
-import urllib.parse
-import xml.etree.ElementTree as ET
 from datetime import date, timedelta
 
 import pandas as pd
-import requests
 from dotenv import load_dotenv
 from google.cloud import bigquery
 
+from request_crossmall import request_crossmall
+
 load_dotenv()
 
-endpoint = "https://crossmall.jp/"
-secret_key = os.environ["SECRET_KEY"]
 company_code = os.environ["COMPANY_CODE"]
 
 today = date.today()
 
 bq_client = bigquery.Client(project="doctor-ilcsi")
-
-
-def request_crossmall(
-    path: str, params: dict, response_root: str, item_root_list=[]
-) -> dict:
-    # URLをエンコード
-    encoded_params = urllib.parse.urlencode(params)
-
-    # 署名の生成
-    def generate_md5_hash(input_string):
-        # ハッシュオブジェクトを作成
-        md5_hash = hashlib.md5()
-        # 入力文字列をバイト型にエンコードしてハッシュオブジェクトに渡す
-        md5_hash.update(input_string.encode("utf-8"))
-        # ハッシュ値を16進数の文字列で取得
-        return md5_hash.hexdigest()
-
-    signing_string = encoded_params + secret_key
-    signing = generate_md5_hash(signing_string)
-
-    url = f"{endpoint}webapi2/{path}?{encoded_params}&signing={signing}"
-    try:
-        response = requests.get(url=url)
-    except:
-        raise ConnectionError(f"CrossmallのAPIでエラー発生。params: {params}")
-    if response.status_code != 200:
-        raise ConnectionError(f"CrossmallのAPIでエラー発生。params: {params}")
-
-    root = ET.fromstring(response.text)
-
-    if item_root_list == ["None"]:
-        # stock値を取得
-        stock_value = root.find(f".//{response_root}")
-        if stock_value is not None:
-            return stock_value.text
-        else:
-            return f"{response_root}要素が見つかりませんでした"
-    else:
-        response_list = []
-        for item in root.findall(f".//{response_root}"):
-            temp_list = []
-            for item_root in item_root_list:
-                name = item.find(item_root).text
-                temp_list.append({item_root: name})
-            merged_dict = {}
-            for d in temp_list:
-                merged_dict.update(d)
-            response_list.append(merged_dict)
-        return response_list
 
 
 def main(args):
