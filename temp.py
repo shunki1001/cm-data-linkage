d2cb5931b502cd577f4f211a71e2be20758f06ae
Shunki <tajimura0505@gmail.com>
2024-07-22 13:32:34 +0900

    商品登録の整合性確認コードの追加

---
+++ temp.py
@@ -0,0 +1,52 @@
+# %%
+import pandas as pd
+
+df1 = pd.read_csv("mapping_csv/登録商品一覧 - CM内のセット品.csv").rename(
+    columns={"親商品の商品管理番号": "商品管理番号"}
+)
+df2 = pd.read_csv(
+    "mapping_csv/登録商品一覧 - RMSからダウンロードしたデータ.csv"
+).rename(columns={"商品管理番号（商品URL）": "商品管理番号"})
+df3 = pd.read_csv("mapping_csv/登録商品一覧 - 対応表.csv")
+
+df1.head()
+df2.head()
+df3.head()
+# %%
+merged_df = pd.merge(df2, df3, on="商品管理番号")
+
+# Value1とValue2列が一致するレコードを抽出
+matching_records = merged_df[
+    merged_df["属性1名"] == merged_df["バリエーション項目選択肢1"]
+].reset_index(drop=True)
+
+matching_records = matching_records[
+    [
+        "商品管理番号",
+        "属性1名",
+        "JANコード",
+        "システム連携用SKU番号",
+        "バリエーション項目選択肢1",
+    ]
+]
+matching_records.to_csv("output.csv")
+
+# %%
+merged_df = pd.merge(df1, df2, on="商品管理番号")
+
+# Value1とValue2列が一致するレコードを抽出
+matching_records = merged_df[
+    merged_df["親属性１名"] == merged_df["バリエーション項目選択肢1"]
+].reset_index(drop=True)
+
+matching_records = matching_records[
+    [
+        "商品管理番号",
+        "構成品属性１名",
+        "親属性１名",
+        "JANコード",
+        "システム連携用SKU番号",
+        "バリエーション項目選択肢1",
+    ]
+]
+matching_records.to_csv("output2.csv")
